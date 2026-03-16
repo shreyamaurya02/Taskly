5ed3c3eccd874aacb1c9a79ece7d51fc78908b9d
SHREYA MAURYA <mauryashreya09d@gmail.com>
2026-03-16 19:58:30 +0530

    fix api url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios"
-
+console.log("API URL:", process.env.REACT_APP_API_URL)
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
-
+console.log("API URL:", process.env.REACT_APP_API_URL)
 // 🔐 Attach JWT to every request
 API.interceptors.request.use(
   (config) => {
